62a88a19c7f41169b2f624520b8f0ab9b18f007f
Harshal Khairnar <khairnar2960@gmail.com>
2023-06-24 11:51:57 +0530

    javascript

---
+++ script.js
@@ -0,0 +1,75 @@
+let name = 'Sonika';
+let address = `Hello
+Welcome             
+	this is tabbed line`;
+const staff = [
+	'Saad',
+	'Irfan'
+];
+const staffObj = {
+	name: 'Saad',
+	age: 21
+};
+function sayHello(name) {
+	return 'Hello ' + name;
+}
+let i = 30;
+
+/*
+// Data types
+Number
+String
+Object/Array
+undefined
+null
+NaN
+function
+boolean
+*/
+
+/*
+#Loops
+for (let i = 0; i < 20; i++) {
+	console.log(i)
+}
+
+while (i < 30) {
+	console.log(i)
+	i++;
+}
+
+for (key in staffObj) {
+	console.log(key)
+}
+for (el of staff) {
+	console.log(el)
+}
+*/
+
+// #1 Conditional statements
+/*
+if (i == 20) {
+	console.log('i is '+ i);
+} else if (i > 30) {
+	console.log('i is greater than 30');
+} else if (i > 20) {
+	console.log('i is greater than 20');
+} else {
+	console.log('i is less than 20');
+}
+
+switch (i) {
+	case 20:
+		console.log('i is 20');
+		break;
+	case 30:
+		console.log('i is 30');
+		break;
+	case 10:
+		console.log('i is 10');
+		break;
+	default:
+		console.log('i is ' + i);
+		break;
+}
+*/
